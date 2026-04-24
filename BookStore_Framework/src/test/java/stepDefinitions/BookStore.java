ed22da32f1ff5c9aff3dba91a1438e1220023f8c
Ganesh <krishganesh2004@gmail.com>
2026-04-24 19:46:52 +0530

    BookStore/Book_StepDefinition_Updated

--- BookStore_Framework/src/test/java/stepDefinitions/BookStore.java
+++ BookStore_Framework/src/test/java/stepDefinitions/BookStore.java
@@ -1,238 +1,73 @@
 package stepDefinitions;
 
 import static io.restassured.RestAssured.given;
-import static org.testng.Assert.assertEquals;
 
-import io.cucumber.java.en.*;
-import io.restassured.RestAssured;
+import hooks.Hooks;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import utils.ConfigReader;
+import utils.ResponseValidator;
 
 public class BookStore {
 
-    Response response;
-    String requestBody;
+    static String isbn;
+    static String secondIsbn;
+    static final String invalidIsbn = "12345";
+    static String replaceIsbn; // target ISBN for PUT body
 
-    String baseUrl = "https://bookstore.toolsqa.com";
+    private void fetchISBNs() {
+        Response res = given().when().get(ConfigReader.get("getBooks"));
+        isbn       = res.jsonPath().getString("books[0].isbn");
+        secondIsbn = res.jsonPath().getString("books[1].isbn");
+        replaceIsbn = secondIsbn;
+    }
 
-    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IkJvb2tTdG9yZTE2MDcyNTYiLCJwYXNzd29yZCI6IlBhc3N3b3JkIzEwIiwiaWF0IjoxNzc2NzQyMDQxfQ.Dc-pIPkuv-TIyP-mSoYjH6y4FVNdgaSrFRNZnCj5uwU";
-    String userId = "96b6339e-2ec8-4d64-9b2d-bd99ae62ed8b";
+    private void setupUserAndToken() {
+        Account.username = "auto_" + System.currentTimeMillis();
+        String body = "{ \"userName\": \"" + Account.username
+                + "\", \"password\": \"Test@123\" }";
+        Response r = given().contentType(ContentType.JSON).body(body)
+                .when().post(ConfigReader.get("createUser"));
+        if (r.getContentType() != null && r.getContentType().contains("json")) {
+            String uid = r.jsonPath().getString("userID");
+            if (uid != null) Account.userId = uid;
+        }
+        r = given().contentType(ContentType.JSON).body(body)
+                .when().post(ConfigReader.get("generateToken"));
+        if (r.getContentType() != null && r.getContentType().contains("json")) {
+            String t = r.jsonPath().getString("token");
+            if (t != null && !t.isEmpty()) Account.token = t;
+        }
+        Hooks.sc.set("userId",Account.userId);
+        Hooks.sc.set("token", Account.token);
+    }
 
-    String originalToken = token;
+    private void addBookToUser(String isbnToAdd) {
+        if (isbnToAdd == null || isbnToAdd.isEmpty()) return;
+        String body = "{ \"userId\": \"" + Account.userId
+                + "\", \"collectionOfIsbns\": [{ \"isbn\": \"" + isbnToAdd + "\" }] }";
+        given().contentType(ContentType.JSON)
+               .header("Authorization", "Bearer " + Account.token)
+               .body(body).when().post(ConfigReader.get("addBook"));
+    }
 
-    String isbn;
-    String secondIsbn;
-    String invalidIsbn = "12345";
+  
 
     @Given("The BookStore API base URL is set")
-    public void setBaseURL() {
-        RestAssured.baseURI = baseUrl;
+    public void setBaseURI() {
+        io.restassured.RestAssured.baseURI = ConfigReader.get("baseUrl");
     }
 
-
-    public void fetchISBNs() {
-        Response res = given().when().get("/BookStore/v1/Books");
-
-        isbn = res.jsonPath().getString("books[0].isbn");
-        secondIsbn = res.jsonPath().getString("books[1].isbn");
-    }
-
-    public void cleanUserBooks() {
-        given()
-            .header("Authorization", "Bearer " + token)
-        .when()
-            .delete("/BookStore/v1/Books?UserId=" + userId);
-    }
-
-    public void addBookToUser() {
-
-        requestBody = "{ \"userId\": \"" + userId + "\"," +
-                "\"collectionOfIsbns\": [{ \"isbn\": \"" + isbn + "\" }] }";
-
-        Response res = given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(requestBody)
-        .when()
-                .post("/BookStore/v1/Books");
-
-        if (res.getStatusCode() != 201) {
-            throw new RuntimeException("Book not added. Status: " + res.getStatusCode());
-        }
-    }
-
+   
     @When("I send a GET request to fetch all books")
     public void getAllBooks() {
-        response = given().when().get("/BookStore/v1/Books");
+        Account.response = given().when().get("/BookStore/v1/Books");
     }
 
     @Given("valid ISBN is available")
     public void validISBN() {
         fetchISBNs();
     }
-
-    @Given("invalid ISBN is prepared")
-    public void invalidISBN() {
-        isbn = invalidIsbn;
-    }
-
-    @When("I send a GET request to fetch book by ISBN")
-    public void getBookByISBN() {
-        response = given()
-                .queryParam("ISBN", isbn)
-        .when()
-                .get("/BookStore/v1/Book");
-    }
-
-    @Given("valid token and ISBN are available")
-    public void validTokenISBN() {
-        fetchISBNs();
-    }
-
-    @Given("valid token and multiple ISBNs are available")
-    public void validMultipleISBN() {
-        fetchISBNs();
-    }
-
-    @Given("valid token and invalid ISBN are available")
-    public void invalidISBNAdd() {
-        isbn = invalidIsbn;
-    }
-
-    @When("I send a POST request to add a book")
-    public void addBook() {
-
-        requestBody = "{ \"userId\": \"" + userId + "\"," +
-                "\"collectionOfIsbns\": [{ \"isbn\": \"" + isbn + "\" }] }";
-
-        response = given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(requestBody)
-        .when()
-                .post("/BookStore/v1/Books");
-    }
-
-    @When("I send a POST request to add multiple books")
-    public void addMultipleBooks() {
-
-        requestBody = "{ \"userId\": \"" + userId + "\"," +
-                "\"collectionOfIsbns\": [" +
-                "{ \"isbn\": \"" + isbn + "\" }," +
-                "{ \"isbn\": \"" + secondIsbn + "\" } ] }";
-
-        response = given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(requestBody)
-        .when()
-                .post("/BookStore/v1/Books");
-    }
-
-  
-
-    @Given("valid token userId and ISBN are available")
-    public void setupForUpdate() {
-
-        cleanUserBooks();
-        fetchISBNs();
-        addBookToUser();
-    }
-
-    @Given("valid token userId and same ISBN are available")
-    public void sameISBN() {
-        setupForUpdate();
-        secondIsbn = isbn;
-    }
-
-    @Given("valid token userId and invalid ISBN are available for update")
-    public void invalidUpdate() {
-        setupForUpdate();
-        secondIsbn = invalidIsbn;
-    }
-
-    @Given("invalid token with valid userId and ISBN are available")
-    public void invalidTokenUpdate() {
-        setupForUpdate();
-        token = "invalid_token";
-    }
-
-    @When("I send a PUT request to replace book")
-    public void replaceBook() {
-
-        requestBody = "{ \"userId\": \"" + userId + "\"," +
-                "\"isbn\": \"" + secondIsbn + "\" }";
-
-        response = given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(requestBody)
-        .when()
-                .put("/BookStore/v1/Books/" + isbn);
-
-        token = originalToken; // restore
-    }
-
-   
-
-    @Given("valid token userId and invalid ISBN are available for delete")
-    public void invalidDelete() {
-        isbn = invalidIsbn;
-    }
-
-    @When("I send a DELETE request to remove book")
-    public void deleteBook() {
-
-        requestBody = "{ \"isbn\": \"" + isbn + "\", \"userId\": \"" + userId + "\" }";
-
-        response = given()
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(requestBody)
-        .when()
-                .delete("/BookStore/v1/Book");
-    }
-
-    @When("I send a DELETE request to remove book again")
-    public void deleteAgain() {
-        deleteBook();
-    }
-
-    @When("I verify the user still exists")
-    public void verifyUserExists() {
-
-        response = given()
-                .header("Authorization", "Bearer " + token)
-        .when()
-                .get("/Account/v1/User/" + userId);
-    }
-
-  
-    @Given("valid userId without token is available")
-    public void noToken() {
-        token = "";
-    }
-
-    @Given("valid token and userId are available")
-    public void validDeleteAll() {
-        token = originalToken;
-    }
-
-    @When("I send a DELETE request to remove all books")
-    public void deleteAllBooks() {
-
-        response = given()
-                .header("Authorization", "Bearer " + token)
-        .when()
-                .delete("/BookStore/v1/Books?UserId=" + userId);
-    }
-
-
-    @Then("the response status should be {int}")
-    public void validateStatus(Integer expectedStatusCode) {
-
-        System.out.println("Response:\n" + response.asPrettyString());
-
-        assertEquals(response.getStatusCode(), expectedStatusCode.intValue());
-    }
-}
