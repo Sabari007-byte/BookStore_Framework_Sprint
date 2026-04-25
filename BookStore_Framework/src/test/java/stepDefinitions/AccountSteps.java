d94ff0af74e7b326b9e92a94ab0273f9f6d604e2
Arish <arishxbma@gmail.com>
2026-04-25 10:22:53 +0530

    Account/Account_DDT_Updated

--- BookStore_Framework/src/test/java/stepDefinitions/AccountSteps.java
+++ BookStore_Framework/src/test/java/stepDefinitions/AccountSteps.java
@@ -10,12 +10,12 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import utils.ConfigReader;
+import config.ConfigReader;
 import utils.ExcelReader;
 import utils.ResponseValidator;
-import utils.ScenarioContext;
 
-public class Account {
+
+public class AccountSteps {
 
     public static Response response;
     public static String   userId;
